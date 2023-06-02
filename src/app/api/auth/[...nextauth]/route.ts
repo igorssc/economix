77dc19e07ffdc157742fe3bea5f721731c16d9cf
Igor Santos <igorsantoscosta@gmail.com>
2023-06-02 15:11:00 -0300

    ci: Finishing Integration of next-auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,13 +6,6 @@ const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
     }),
   ],
 };
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 
 export default function Login() {
   const { data: session, status } = useSession();
@@ -19,7 +20,23 @@ export default function Login() {
 
   return (
     <>
-      <p>Você está autenticado como {session?.user?.email}.</p>
+      <h1>Bem vindo, {session.user?.name}</h1>
+      {session.user?.image && (
+        <Image
+          alt="Image Profile"
+          src={session.user.image}
+          width={50}
+          height={50}
+        />
+      )}
+      <p>
+        Você está autenticado como: {session.user?.email}, até:{" "}
+        {new Date(session.expires).toLocaleDateString("pt-BR", {
+          hour: "numeric",
+          minute: "numeric",
+          second: "numeric",
+        })}
+      </p>
       <button onClick={() => signOut()}>Sair</button>
     </>
   );
